2e563f9a486ab7af57b21533a3533ce236346da8
sanjid133 <sanjid@appscode.com>
2017-08-18 18:41:48 +0600

    initial out-of-tree provider

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
-//go:generate go-extpoints ../../pkg/provider/extpoints
 package main
 
 import (
+	"fmt"
 	"os"
 
-	_ "github.com/appscode/krank/cloud/providers"
-	"github.com/appscode/krank/cmds"
-	logs "github.com/appscode/log/golog"
+	"github.com/appscode/krank/cloud"
+	"github.com/golang/glog"
+	"github.com/spf13/pflag"
+	"k8s.io/apiserver/pkg/server/healthz"
+	"k8s.io/apiserver/pkg/util/flag"
+	"k8s.io/apiserver/pkg/util/logs"
+	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
+	"k8s.io/kubernetes/cmd/cloud-controller-manager/app/options"
+	_ "k8s.io/kubernetes/pkg/client/metrics/prometheus" // for client metric registration
+	"k8s.io/kubernetes/pkg/cloudprovider"
+	_ "k8s.io/kubernetes/pkg/cloudprovider/providers"
+	_ "k8s.io/kubernetes/pkg/version/prometheus" // for version metric registration
+	"k8s.io/kubernetes/pkg/version/verflag"
 )
 
+func init() {
+	healthz.DefaultHealthz()
+}
+
 func main() {
+	s := options.NewCloudControllerManagerServer()
+	s.AddFlags(pflag.CommandLine)
+
+	flag.InitFlags()
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	if err := cmds.NewCmdStartup().Execute(); err != nil {
+	verflag.PrintAndExitIfRequested()
+
+	cloud, err := cloudprovider.InitCloudProvider(cloud.ProviderName, s.CloudConfigFile)
+	if err != nil {
+		glog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+
+	if err := app.Run(s, cloud); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	os.Exit(0)
 }
